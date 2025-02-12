8f873887ffb2f5379407774e420a766b8717f656
Pan <taufan.maaruf@gmail.com>
2025-02-12 10:38:03 +0700

    finished UVa 01709 - Amalgamated Artichokes problem

--- competitive-programming-4-book/chapter-1/Function/5-UVa 01709 - Amalgamated Artichokes.cpp
+++ competitive-programming-4-book/chapter-1/Function/5-UVa 01709 - Amalgamated Artichokes.cpp
@@ -6,18 +6,18 @@ int main () {
     double p, a, b, c, d;
     int n;
     while (cin >> p >> a >> b >> c >> d >> n) {
-        long double price[n];
+        double price[n];
         for (int i = 0 ; i < n ; i++) {
             price[i] = p * (sin(a * (i + 1) + b) + cos(c * (i + 1) + d) + 2);
         }
 
-        long double maxVal = price[0];
-        long double minVal = 0;
-        for (int i = 1 ; i < n - 1; i++) {
+        double maxVal = price[0];
+        double minVal = 0;
+        for (int i = 1 ; i < n; i++) {
             minVal = min(minVal, price[i] - maxVal);
             maxVal = max(maxVal, price[i]);
         }
 
-        printf("%.6Lf\n", abs(minVal));
+        printf("%.9lf\n", abs(minVal));
     }
 }
