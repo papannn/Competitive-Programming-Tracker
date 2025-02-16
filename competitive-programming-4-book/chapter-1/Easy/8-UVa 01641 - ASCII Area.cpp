96173d8c72bd4d1f30ab147141ca7b44ddd0da11
Pan <taufan.maaruf@gmail.com>
2025-02-16 20:29:15 +0700

    finished UVa 01641 - ASCII Area problem

--- competitive-programming-4-book/chapter-1/Easy/8-UVa 01641 - ASCII Area.cpp
+++ competitive-programming-4-book/chapter-1/Easy/8-UVa 01641 - ASCII Area.cpp
@@ -3,26 +3,28 @@
 using namespace std;
 int main () {
     int h, w;
-    cin >> h >> w;
-    string s[h];
-    for (int i = 0 ; i < h; i++) {
-        cin >> s[i];
-    }
+    while (cin >> h >> w) {
+        string s[h];
+        for (int i = 0 ; i < h; i++) {
+            cin >> s[i];
+        }
 
-    int sum = 0;
-    for (int i = 0 ; i < h; i++) {
-        bool open = false;
-        for (int j = 0 ; j < w; j++) {
-            if (s[i][j] == '.' && open) {
-                sum += 2;
-            }
+        int sum = 0;
+        for (int i = 0 ; i < h; i++) {
+            bool open = false;
+            for (int j = 0 ; j < w; j++) {
+                if (s[i][j] == '.' && open) {
+                    sum += 2;
+                }
 
-            if (s[i][j] == '/' || s[i][j] == '\\') {
-                sum += 1;
-                open = !open;
+                if (s[i][j] == '/' || s[i][j] == '\\') {
+                    sum += 1;
+                    open = !open;
+                }
             }
         }
-    }
 
-    cout << sum / 2 << endl;
+        cout << sum / 2 << endl;
+    }
+    
 }
