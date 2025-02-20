c07803e614d2b329508cccda7893f24c88105b5b
Pan <taufan.maaruf@gmail.com>
2025-02-20 22:40:51 +0700

    finished Kattis - bossbattle problem

--- competitive-programming-4-book/chapter-1/Still Easy/5-Kattis - bossbattle.cpp
+++ competitive-programming-4-book/chapter-1/Still Easy/5-Kattis - bossbattle.cpp
@@ -4,13 +4,5 @@ using namespace std;
 int main () {
     int n;
     cin >> n;
-    int pos = 2;
-    int bossPos = n - 1;
-    int res = 0;
-    while (abs(bossPos % n - pos % n) > 0) {
-        bossPos++;
-        pos += 2;
-        res++;
-    }
-    cout << res + 1 << endl;
+    cout << (n - 2 > 0 ? n - 2 : 1) << endl;
 }
