5bb6f3e7be73025fe870246715866ebe9c32ca6a
Pan <taufan.maaruf@gmail.com>
2025-02-28 19:36:21 +0700

    finished Kattis - cowcrane problem

--- competitive-programming-4-book/chapter-1/Medium/19-Kattis - cowcrane.cpp
+++ competitive-programming-4-book/chapter-1/Medium/19-Kattis - cowcrane.cpp
@@ -1,79 +1,30 @@
 #include <bits/stdc++.h>
+#define ll long long
 
 using namespace std;
+
+int diff(long long a, long long b) {
+    return abs(a - b);
+}
+
 int main () {
-    // Init
-    long long cow1, cow2, target1, target2, timeLimit1, timeLimit2;
+    ll cow1, cow2;
+    ll target1, target2;
+    ll time1, time2;
+
     cin >> cow1 >> cow2;
     cin >> target1 >> target2;
-    cin >> timeLimit1 >> timeLimit2;
-    long long originalCow1 = cow1;
-    long long originalCow2 = cow2;
-    long long duration1 = 0;
-    long long duration2 = 0;
-    bool possible1 = true;
-    bool possible2 = true;
-
-
-    // 
-    duration1 += abs(cow1) + abs(max(cow1, target1) - min(cow1, target1));
-    // cout << "Duration 1: " << duration1 << endl; 
-    possible1 &= duration1 <= timeLimit1;
-    long long tempCow2 = cow2;
-    if (cow1 >= 0 && (cow2 >= 0 && cow2 <= cow1)) {
-        if (target2 <= cow1 && cow2 <= target2) {
-            cow2 = target2;
-        } else {
-            cow2 = cow1;
-        }
-    } else if (cow1 <= 0 && (cow2 <= 0 && cow2 >= cow1)) {
-        if (target2 >= cow1 && cow2 >= target2) {
-            cow2 = target2;
-        } else {
-            cow2 = cow1;
-        }
+    cin >> time1 >> time2;
+    bool possible = false;
+    if (diff(0, cow1) + diff(cow1, target1) <= time1 && diff(0, cow1) + diff(cow1, target1) + diff(target1, cow2) + diff(cow2, target2) <= time2) {
+        possible = true;
+    } else if (diff(0, cow2) + diff(cow2, target2) <= time2 && diff(0, cow2) + diff(cow2, target2) + diff(target2, cow1) + diff(cow1, target1) <= time1) {
+        possible = true;
+    } else if (diff(0, cow2) + diff(cow2, cow1) + diff(cow1, target1) <= time1 && diff(0, cow2) + diff(cow2, cow1) + diff(cow1, target1) + diff(target1, cow1) + diff(cow1, target2) <= time2) {
+        possible = true;
+    } else if (diff(0, cow1) + diff(cow1, cow2) + diff(cow2, target2) <= time2 && diff(0, cow1) + diff(cow1, cow2) + diff(cow2, target2) + diff(target2, cow2) + diff(cow2, target1) <= time1) {
+        possible = true;
     }
 
-    // if (cow1 >= 0 && (tempCow2 >= 0 && tempCow2 <= target1)) {
-    //     cow2 = target1;
-    // } else if (cow1 <= 0 && (tempCow2 <= 0 && tempCow2 >= target1)) {
-    //     cow2 = target1;
-    // }
-    duration1 += abs(max(target1, cow2) - min(target1, cow2)) + abs(max(cow2, target2) - min(cow2, target2));
-    // cout << "Duration 1: " << duration1 << endl; 
-    possible1 &= duration1 <= timeLimit2;
-
-    // cout << "Duration 1: " << duration1 << endl; 
-    cow2 = originalCow2;
-    duration2 += abs(cow2) + abs(max(cow2, target2) - min(cow2, target2));
-    // cout << "Duration 2: " << duration2 << endl;
-    possible2 &= duration2 <= timeLimit2;
-    long long tempCow1 = cow1;
-    if (cow2 >= 0 && (cow1 >= 0 && cow1 <= cow2)) {
-        if (target1 <= cow2 && cow1 <= target1) {
-            cow1 = target1;
-        } else {
-            cow1 = cow2;
-        }
-    } else if (cow2 <= 0 && (cow1 <= 0 && cow1 >= cow2)) {
-        if (target1 >= cow2 && cow1 >= target1) {
-            cow1 = target1;
-        } else {
-            cow1 = cow2;
-        }
-        
-    }
-
-    // if (cow2 >= 0 && (tempCow1 >= 0 && tempCow1 <= target2)) {
-    //     cow1 = target2;
-    // } else if (cow2 <= 0 && (tempCow1 <= 0 && tempCow1 >= target2)) {
-    //     cow1 = target2;
-    // }
-    duration2 += abs(max(target2, cow1) - min(target2, cow1)) + abs(max(cow1, target1) - min(cow1, target1));
-    // cout << "Duration 2: " << duration2 << endl;
-    possible2 &= duration2 <= timeLimit1;
-
-    // cout << "Duration 2: " << duration2 << endl;
-
-    cout << ((possible1 || possible2) ?  "possible" : "impossible") << endl;
+    cout << (possible ? "possible" : "impossible") << endl;
 }
