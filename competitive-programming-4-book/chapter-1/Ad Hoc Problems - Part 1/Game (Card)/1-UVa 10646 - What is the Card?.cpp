1ce994d1426052e9b67d8c7bddb39fff77820d29
Pan <taufan.maaruf@gmail.com>
2025-03-01 13:00:57 +0700

    finished UVa 10646 - What is the Card? problem (Not verified)

---
+++ competitive-programming-4-book/chapter-1/Ad Hoc Problems - Part 1/Game (Card)/1-UVa 10646 - What is the Card?.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main () {
+    int t;
+    cin >> t;
+    vector<string> cards;
+    int count = 1;
+    while (t--) {
+        int amountCard = 52;
+        while (amountCard--) {
+            string card;
+            cin >> card;
+            cards.push_back(card);
+        }
+
+        deque<string> hand;
+        int amountHand = 25;
+        while (amountHand--) {
+            string card = cards[cards.size() - 1];
+            hand.push_front(card);
+            cards.pop_back();
+        }
+        
+        int y = 0;
+        for (int i = 0 ; i < 3; i++) {
+            string card = cards[cards.size() - 1];
+            cards.pop_back();
+            int x = isdigit(card[0]) ? card[0] - '0' : 10;
+            y += x;
+            for (int j = 0; j < 10 - x; j++) {
+                cards.pop_back();
+            }
+        }
+
+        while (hand.size() > 0) {
+            string card = hand[0];
+            cards.push_back(card);
+            hand.pop_front();
+        }
+
+        cout << "Case " << count++ << ": ";
+        cout << cards[y - 1] << endl;
+    }
+}
