43466c367f7328a3e5ce3ce1373dbfe74ce40ecc
Pan <taufan.maaruf@gmail.com>
2025-04-20 15:11:15 +0700

    TLE Eliminator: finished Watermelon problem

---
+++ codeforces/TLE Eliminator/Level 1/Module-1:C++ Basics/Day 5/Watermelon.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main () {
+    int x;
+    cin >> x;
+    if (x % 2 == 1) { // Odd
+        cout << "NO" << endl;
+    } else  if (x % 2 == 0 && x != 2) { // Odd and not 2
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
