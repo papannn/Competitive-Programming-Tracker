f15995d4d453d3fa27bdc9b9563d52db0adbce08
Pan <taufan.maaruf@gmail.com>
2025-05-18 20:17:31 +0700

    finished codeforces (800) - Soft Drinking problem

---
+++ codeforces/Problemset/800/52-A. Soft Drinking.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+// https://codeforces.com/problemset/problem/151/A
+int main () {
+    int n, k, l, c, d, p, nl, np;
+    cin >> n >> k >> l >> c >> d >> p >> nl >> np;
+    int lime = c * d;
+    int drink = k * l / nl;
+    int salt = p / np;
+    cout << min(salt, min(lime, drink)) / n << endl;
+}
